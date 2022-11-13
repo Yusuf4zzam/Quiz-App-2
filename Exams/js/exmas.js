14197abaa3fab64a74da8ac0ea7ff5e5949a5722
yusuf <yossefazzam2@gmail.com>
2022-11-13 17:26:12 +0200

    More Changes

--- Exams/js/exmas.js
+++ Exams/js/exmas.js
@@ -27,7 +27,7 @@ burgerToggleBtn.addEventListener("click", () => {
 });
 
 /* -------------------------------------- Fetching The Data  -------------------------------------- */
-fetch("Exams/Exams-Data.json")
+fetch("./Exams-Data.json")
   .then((res) => res.json())
   .then((data) => createCards(data));
 
